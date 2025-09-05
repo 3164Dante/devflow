e7f1444f829381aacb743d93c9aae8a99dd736b6
Dante Angel <dante.angel.85@gmail.com>
2025-09-04 19:25:52 -0600

    Implemnet ESLint & Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 const page = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">Welcome to Next js 🤗</h1>
+      <h1 className="text-3xl text-violet-700 font-black">
+        Welcome to Next js 🤗
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
